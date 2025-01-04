c74fb2b6f6a3c7a73e21a2504e1edbf9415d6adb
Rizalda <rizaldadickx@gmail.com>
2025-01-04 17:29:48 +0700

    build production

--- app.py
+++ app.py
@@ -10,10 +10,19 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# Load configuration based on FLASK_ENV
+if os.getenv('FLASK_ENV') == 'production':
+    app.config['DEBUG'] = False
+    app.config['ENV'] = 'production'
+else:
+    app.config['DEBUG'] = True
+    app.config['ENV'] = 'development'
+
 # Load dataset
 data_file = os.getenv('DATA_FILE')
 news_data = pd.read_csv(data_file)
 index_csv(data_file)
+
 
 @app.route('/news', methods=['GET'])
 def get_news():
@@ -21,24 +30,26 @@ def get_news():
     per_page = 10
     start = (page - 1) * per_page
     end = start + per_page
-    
+
     # Get category from query parameters
     category = request.args.get('category', default=None, type=str)
 
     # Filter news data by category if provided
     if category:
-        filtered_news = news_data[news_data['category'].str.lower() == category.lower()]
+        filtered_news = news_data[news_data['category'].str.lower(
+        ) == category.lower()]
     else:
         filtered_news = news_data
 
     paginated_news = filtered_news[start:end].to_dict(orient='records')
-    
+
     return jsonify({
         'page': page,
         'per_page': per_page,
         'total': len(filtered_news),
         'news': paginated_news
     })
+
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -59,5 +70,6 @@ def search():
         'news': paginated_results
     })
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
