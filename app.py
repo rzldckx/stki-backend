7b84d67263d607d4ce6ef5bae4288553967edab1
Rizalda <rizaldadickx@gmail.com>
2025-01-05 09:46:26 +0700

    stop index whoosh

--- app.py
+++ app.py
@@ -99,8 +99,8 @@ def search_news():
 
 
 if __name__ == '__main__':
-    print("Starting Flask application...")
-    index_db_background()  # Start indexing in the background
+    # print("Starting Flask application...")
+    # index_db_background()  # Start indexing in the background
     port = int(os.getenv('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
     print("Flask application is running.")
